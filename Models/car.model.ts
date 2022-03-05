19a81b3128ed7e19b498cba2d2328da41cf3ee51
pavithran2914 <pavithranbala29@gmail.com>
2022-03-05 12:21:10 +0530

    2 files modified

--- Models/car.model.ts
+++ Models/car.model.ts
@@ -9,17 +9,24 @@ export class Car {
   bolt: Bolt;
   carDoor: CarDoor;
   carEngine: CarEngine;
-  constructor(carNo: number, carBrand: string, color: string, bolt: Bolt, carDoor: CarDoor, carEngine: CarEngine) {
+  constructor(carNo: number, carBrand: string, color: string) {
     this.carNo = carNo;
     this.carBrand = carBrand;
     this.color = color;
-    this.bolt = bolt;
-    this.carDoor = carDoor;
-    this.carEngine = carEngine;
+    this.carDetial();
   }
   feature() {
     console.log(
       `This car 0 to 90 in 30 sec.\n Automatic Climate Control.\n Music System.`
     );
+  }
+  carDetial() {
+    let bt: Bolt = new Bolt('castle', 'one', 20);
+    let engine: CarEngine = new CarEngine(bt, 5, 2);
+    let door: CarDoor = new CarDoor(2, 2, 2, 'steel & coting fribric', 'new Type', bt);
+    bt.boltFunction();
+    engine.engineFunction();
+    door.doorFunction();
+    console.log(bt, engine, door);
   }
 }
--- index.ts
+++ index.ts
@@ -1,26 +1,16 @@
 // Import stylesheets
 import './style.css';
 import { Car } from './Models/car.model';
-import { Bolt } from './Models/bolt.model';
-import { CarDoor } from './Models/carDoor.model';
-import { CarEngine } from './Models/carEngine.model';
+
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
-let bt = new Bolt("castle","one", 20);
-
-let engine = new CarEngine(bt, 5, 2);
-
-let door = new CarDoor(2, 2, 2, "steel & coting fribric", "new Type", bt);
-
-let carobj = new Car(1524,"BMW", "Made Black", bt, door, engine);
+let carobj = new Car(1524,"BMW", "Made Black");
 
 console.log(carobj);
 
-bt.boltFunction();
-engine.engineFunction();
-door.doorFunction();
+
 carobj.feature();
 
